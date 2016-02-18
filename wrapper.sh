4c81503e74a56def57d93ead3f26990e11643a6e
James Hall <gymboh32@gmail.com>
2016-02-18 02:17:55 +0000

    changed working directory

--- wrapper.sh
+++ wrapper.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-WALLS_DIR="$HOME/Pictures/todayswalls"
+WALLS_DIR="/home/jahall/ragebox/todayswalls"
 
 # Move scripts over and create rolling wallpaper
 #
-cp ~/ragebox/git/wallpapers/createSlideshow.sh ~/Pictures/todayswalls/createSlideshow.sh
-~/Pictures/todayswalls/createSlideshow.sh 15
+cp ~/ragebox/git/wallpapers/createSlideshow.sh "${WALLS_DIR}"/createSlideshow.sh
+cd "${WALLS_DIR}"
+./createSlideshow.sh 15
 
 # Clean up the folder
 #
-cd "${WALLS_DIR}"
+#cd "${WALLS_DIR}"
 
 rm link*
 rm *.sh
